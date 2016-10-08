abb6a270695b356ce61853088b9a3f751167d3e1
Jonathan Funk <jonfunk88@hotmail.com>
2016-10-08 09:30:11 -0700

    navigation set up and linked

--- js/jquery.js
+++ js/jquery.js
@@ -7,4 +7,19 @@ $(document).ready(function(){
       alert('Thank you for subscribing!');
     }
   });
+
+  var offset = $("header").height();
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - offset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
